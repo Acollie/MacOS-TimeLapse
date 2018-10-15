3243a6380db454c86fd38dd7cec73f3862ba2b06
acollie <12976663+Acollie@users.noreply.github.com>
2018-10-15 20:53:30 +0100

    More

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# Py-Lapse-GUI
+# MacOS-TimeLapse
 ## Simple program to take desktop screenshots at a timed interval.  
 
+This program, is designed to be able take screenshots at a interval.
+
+
+  
+### Contribution guide  
+You can fork the repo and makes something cool, and I probably will merge it. 
+Otherwise look at the issues and any fixes on that would be greatly appreciated
+
+### Build guide
+
+**Dependencies**  
+python 3.7  
+tkinter
--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 from tkinter import *
+from tkinter import messagebox
 import time
 import os
 
 def loop():
     screenshot_count = 1
     if not os.path.exists('screenshots'):
+        messagebox.showinfo("Information", "Since there is no Screenshots folder. A file has been generated.")
         os.makedirs('screenshots')
+    messagebox.showinfo("Information", "Please note, if you are done with taking screenshots, force quit the program.")
     while True:
 
         n = w1.get()
         time.sleep(n)
-        T.insert(END, "Screenshot "+ str(x)+"\n")
-        os.system("screencapture screenshots/Screenshot_"+str(screenshot_count) + ".png")
+        T.insert(END, "Screenshot "+ str(screenshot_count)+"\n")
+        os.system("screencapture -x screenshots/Screenshot_"+str(screenshot_count) + ".png")
         screenshot_count = screenshot_count + 1
         master.update()
 
 
 master = Tk()
 master.geometry("250x250")
-master.title("Py Lapse")
+master.title("MacOS-TimeLapse")
 w1 = Scale(master, from_=1, to=100,orient=HORIZONTAL)
 w1.set(50)
 w1.pack()
 
 Label(master, text="Stating Loop Scale is in seconds").pack()
+
 Button(master, text='Start Screenshot', command=loop).pack()
 Button(master, text='Quit', command=quit).pack()
 
